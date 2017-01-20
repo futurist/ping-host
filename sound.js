7d169e142f20583cfa676eedde64d7ffee95d8f4
futurist <jamesyang999@gmail.com>
2017-01-20 08:46:32 +0800

    add: sound ping

---
+++ sound.js
@@ -0,0 +1,22 @@
+var ping = require('ping')
+var exec = require('child_process').exec
+
+var host = '192.168.1.1'
+
+var cfg = {
+  timeout: 100
+}
+
+function pingHost () {
+  ping.promise.probe(host, cfg).then(function (res) {
+    // console.log(res.host)
+    console.log((new Date()).toString(), host, res.alive)
+    // console.log(res)
+    if (res.alive) {
+      exec(`afplay "success.mp3"`)
+    }
+  })
+}
+
+var inter1 = setInterval(pingHost, 1000)
+
